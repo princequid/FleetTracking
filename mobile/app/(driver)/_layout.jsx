dd11325ee6e0460cf7eecaa314dd8e48cd4cfdb9
Simon Quarm <Simon>
2026-07-03 01:16:44 +0000

    the mobile view maps with navigations

--- mobile/app/(driver)/_layout.jsx
+++ mobile/app/(driver)/_layout.jsx
@@ -2,6 +2,20 @@ import { Stack } from 'expo-router';
 
 export default function DriverLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }}>
+      {/* Map and camera screens slide up from bottom like modals */}
+      <Stack.Screen
+        name="trip/[id]/map"
+        options={{ animation: 'slide_from_bottom', presentation: 'modal' }}
+      />
+      <Stack.Screen
+        name="delivery/pre-dispatch/[id]_3"
+        options={{ animation: 'slide_from_bottom', presentation: 'modal' }}
+      />
+      <Stack.Screen
+        name="delivery/pod/[id]_3"
+        options={{ animation: 'slide_from_bottom', presentation: 'modal' }}
+      />
+    </Stack>
   );
 }
