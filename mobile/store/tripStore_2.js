8227a737c42f0e9109a76f0b52d78b7bc7e54110
Simon Quarm <Simon>
2026-07-03 01:16:44 +0000

    the mobile view maps with navigations

--- mobile/store/tripStore_2.js
+++ mobile/store/tripStore_2.js
@@ -2,11 +2,11 @@ import { create } from 'zustand';
 
 export const useTripStore = create((set) => ({
   podUploaded: false,
-  activeTrip: null,
+  activeTrip:  null,
 
-  setPodUploaded: (uploaded) => set({ podUploaded: uploaded }),
-
-  setActiveTrip: (trip) => set({ activeTrip: trip }),
+  setPodUploaded: (val)  => set({ podUploaded: val }),
+  setActiveTrip:  (trip) => set({ activeTrip: trip }),
+  resetTripStore: ()     => set({ podUploaded: false, activeTrip: null }),
 }));
 
 export default useTripStore;
