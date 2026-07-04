7fef7f12fc93ebab95b23aa8105af1bf4ea7e4ce
Simon Quarm <Simon>
2026-07-04 09:50:22 +0000

    working on the maps to make sure everything works well

--- backend/notification-service_5/src/main/java/com/fleettrack/notification/model/enums/NotificationType.java
+++ backend/notification-service_5/src/main/java/com/fleettrack/notification/model/enums/NotificationType.java
@@ -1,5 +1,10 @@
 package com.fleettrack.notification.model.enums;
 
-// 
-public class NotificationType {
+public enum NotificationType {
+    TRIP_ASSIGNED,
+    TRIP_STARTED,
+    TRIP_CANCELLED,
+    TRIP_DELIVERED,
+    ALERT,
+    INFO
 }
